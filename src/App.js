bd3a91c2ac39b06d8259aa6a9474bd4dc2331178
Akash Sharma <aakash.sharma413@gmail.com>
2022-10-14 00:49:20 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Restaurent from './component/Basics/Restaurent'
-// import UseState from './component/Hooks/UseState'
+import UseState from './component/Hooks/UseState'
 // import UseEffect from './component/Hooks/UseEffect'
 // import UseReducer from './component/Hooks/UseReducer'
-// import Todo from './component/Todo/Todo'
-import Temp from './component/Weather/temp'
 
 const App = () => {
   return (
-    <Temp />
+    <UseState />
   )
 }
 
